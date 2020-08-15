0d12089acfb4847f603393039c6c74fa65548d94
Seth Mitchell <sayotte.mitchell@gmail.com>
2020-08-15 10:47:56 -0400

    Modals jQuery & Sassy styling

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(function() {
+    $(".carousel").carousel( {interval: 7000});
+    $("#carouselButton").click(function() {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+});
+
+$(function() {
+    $('#reserveButton').click(function() {
+        $('#reserveModal').modal("show");
+    });
+
+    $('#loginButton').click(function() {
+        $('#loginModal').modal("show");
+    });
+});
